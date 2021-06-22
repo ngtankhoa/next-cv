fc4011f7c8354f96c14c093c302757fa63f38a28
khoa <khoant.uit@gmail.com>
2021-06-23 00:18:31 +0700

    ignore eslint cache

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,9 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {' '}
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>{' '}
+      </Head>
       <Component {...pageProps} />
     </>
   );
