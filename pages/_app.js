9aedfdecdad9080f9e226b94c5e8663a35dc1d05
khoa <khoant.uit@gmail.com>
2021-06-22 23:59:51 +0700

    commit app.js by vs code

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import 'antd/dist/antd.css';
 import 'styles/vars.css';
 import 'styles/globals.css';
-import Head from 'next/head'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    return <> <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-        </Head> <Component {...pageProps} /></>;
+  return (
+    <>
+      {' '}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>{' '}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
