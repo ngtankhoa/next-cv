1db68a627188fda7073c1dbf94f72952fe252e91
khoa <khoant.uit@gmail.com>
2021-06-23 00:58:47 +0700

    add quote again

--- containers/summary/summary.tsx
+++ containers/summary/summary.tsx
@@ -8,7 +8,7 @@ export function Summary() {
         <TitleSession>Summary</TitleSession>
         <Card hoverable bordered={false}>
           <Typography.Paragraph>
-            I m a Mid-Level Web Developer specializing in Front-end web and Mobile development.
+            I&apos;m a Mid-Level Web Developer specializing in Front-end web and Mobile development.
             <br />
             In a normal work flow, I receive design from Figma, split them into reusable parts and
             make components base on that.
