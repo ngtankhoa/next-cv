066a3df241ac5a81c727d8e51037519c50684f3f
khoa <khoant.uit@gmail.com>
2021-07-03 03:08:21 +0700

    commit

--- containers/experience/experience.tsx
+++ containers/experience/experience.tsx
@@ -9,6 +9,14 @@ export function Experience() {
       <Card hoverable bordered={false}>
         <Timeline>
           <Timeline.Item
+          // label='06/2020 - Current'
+          >
+            <Text strong>06/2020 - Current</Text>
+            <br />
+            Role: Front-end, Mobile developer <br />
+            Knowledge achievement: React/Next, Typescript, React Native
+          </Timeline.Item>
+          <Timeline.Item
           // label='09/2018 - 09 2019'
           >
             <Text strong>09/2018 - 09 2019</Text>
@@ -16,14 +24,6 @@ export function Experience() {
             Role: Interns, Back-end web developer
             <br />
             Knowledge achievement: HTML, CSS, JavaScript, SailsJS, Arduino, RabbitMQ
-          </Timeline.Item>
-          <Timeline.Item
-          // label='06/2020 - Current'
-          >
-            <Text strong>06/2020 - Current</Text>
-            <br />
-            Role: Front-end, Mobile developer <br />
-            Knowledge achievement: React/Next, Typescript, React Native
           </Timeline.Item>
         </Timeline>
       </Card>
