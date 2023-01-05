610a13a8a26e55a0162d9802c1675de45f9e91d7
John Nguyen <john.nguyen@legato.co>
2023-01-05 21:26:24 +0700

    experience updated

--- containers/summary/summary.tsx
+++ containers/summary/summary.tsx
@@ -8,7 +8,7 @@ export function Summary() {
       <InformationContainer>
         <TitleSession
           rightContent={
-            <a href='https://github.com/ngtankhoa/next-cv' target='_blank' rel="noreferrer">
+            <a href='https://github.com/ngtankhoa/next-cv' target='_blank' rel='noreferrer'>
               View my source code on <GithubOutlined /> Github
             </a>
           }>
@@ -16,7 +16,7 @@ export function Summary() {
         </TitleSession>
         <Card hoverable bordered={false}>
           <Typography.Paragraph>
-            Hi, I&apos;m Nguyen Tan Khoa, a one-year experience Developer specializing in Front-end web and Mobile
+            Hi, I&apos;m Nguyen Tan Khoa, a two-year experience Developer specializing in Front-end web and Mobile
             development.
             <br />
             In a normal work flow, I receive design from Figma, split them into reusable parts and make components base
